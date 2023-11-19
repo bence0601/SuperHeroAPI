367c441b0a7623c80b3ec2c1ccdc233d708c8a50
bence0601 <nagybence0601@gmail.com>
2023-11-19 17:47:09 +0100

    Commit : Created CRUD operations

---
+++ SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI.Models;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private static List<Superhero> SuperHeroes = new List<Superhero>
+        {
+            new Superhero
+                {   ID = 1,
+                    Name = "Spider Man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York" },
+
+            new Superhero
+            {   ID = 2,
+            Name = "Iron Man",
+            FirstName = "Tony",
+            LastName = "Stark",
+            Place = "Malibu" }
+
+        };
+
+        [HttpGet("AllSuperHeroes")]
+        public async Task<ActionResult<List<Superhero>>> GetAllHeroes()
+        {
+
+            return Ok(SuperHeroes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Superhero>> GetSuperHeroById(int id)
+        {
+            var hero = SuperHeroes.Find(x => x.ID == id);
+
+            if (hero == null)
+            {
+                return NotFound("Sorry, this superhero does not exist");
+            }
+
+            return Ok(hero);
+        }
+
+        [HttpPost("AddHero")]
+        public async Task<ActionResult<Superhero>> AddSuperHero(Superhero hero)
+        {
+
+            SuperHeroes.Add(hero);
+            return Ok(hero);
+        }
+
+        [HttpPut("UpdateHero")]
+        public async Task<ActionResult<List<Superhero>>> UpdateSuperHero(int id, Superhero hero)
+        {
+            var oldHero = SuperHeroes.Find(x => x.ID == id);
+            oldHero = hero;
+            //oldHero.ID = hero.ID;
+            //oldHero.Name = hero.Name;
+            //oldHero.FirstName = hero.FirstName;
+            //oldHero.LastName = hero.LastName;
+            //oldHero.Place = hero.Place;
+
+            return Ok(SuperHeroes);
+        }
+    }
+}
---
+++ SuperHeroAPI/SuperHeroAPI/Models/Superhero.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperHeroAPI.Models
+{
+    public class Superhero
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Place { get; set; }
+
+    }
+}
